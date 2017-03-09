323afc16ec63e522c61097d3d2c37fae43f9d807
Rebecca French <frenchrb@jmu.edu>
2017-03-09 13:15:43 -0500

    Initial commit

---
+++ OTL_licenses.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import csv
+import sys
+import re
+
+def main(input, output):
+    browser = webdriver.Firefox()
+
+    outputFile = open(output, 'w')
+    writer = csv.writer(outputFile, doublequote=True, lineterminator='\r')
+
+    with open(input, 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            result = row
+            if 'http' not in row[len(row)-1]:
+                result.append('LICENSE')
+                result.append('506FIELD')
+                writer.writerow(result)
+            else:
+                url = row[len(row)-1]
+                browser.get(url)
+                license = browser.find_element_by_class_name(
+                    'Badge-Condition').text
+                license = str.replace(license, u'\n', ' :: ')
+                license = str.replace(license, u'\r', ' :: ')
+                result.append(license)
+                
+                if "Attribution-NonCommercial-ShareAlike :: CC BY-NC-SA" in license:
+                    field506 = "This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike (CC BY-NC-SA) License.$uhttps://creativecommons.org/licenses/by-nc-sa/4.0/legalcode"
+                elif "Attribution-NonCommercial-NoDerivs :: CC BY-NC-ND" in license:
+                    field506 = "This work is licensed under a Creative Commons Attribution-NonCommercial-NoDerivs (CC BY-NC-ND) License.$uhttps://creativecommons.org/licenses/by-nc-nd/4.0/legalcode"
+                elif "Attribution-NonCommercial :: CC BY-NC" in license:
+                    field506 = "This work is licensed under a Creative Commons Attribution-NonCommercial (CC BY-NC) License.$uhttps://creativecommons.org/licenses/by-nc/4.0/legalcode"
+                elif "Attribution-NoDerivs :: CC BY-ND" in license:
+                    field506 = "This work is licensed under a Creative Commons Attribution-NoDerivs (CC BY-ND) License.$uhttps://creativecommons.org/licenses/by-nd/4.0/legalcode"
+                elif "Attribution-ShareAlike :: CC BY-SA" in license:
+                    field506 = "This work is licensed under a Creative Commons Attribution-ShareAlike (CC BY-SA) License.$uhttps://creativecommons.org/licenses/by-sa/4.0/legalcode"
+                elif "Attribution :: CC BY" in license:
+                    field506 = "This work is licensed under a Creative Commons Attribution (CC BY) License.$uhttps://creativecommons.org/licenses/by/4.0/legalcode"
+                elif "Free Documentation License :: GNU" in license:
+                    field506 = "This work is licensed under a GNU Free Documentation License.$uhttp://www.gnu.org/licenses/fdl.html"
+                else:
+                    field506 = ""
+                result.append(field506)
+                writer.writerow(result)
+
+    browser.quit()
+    outputFile.close()
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2])
---
+++ README.md
@@ -0,0 +1,9 @@
+# OTL_licenses
+
+Script to retrieve license information for Open Textbook Library e-books and format 506 field to be inserted in MARC records
+
+Input: .csv with the e-book URL in the final (rightmost) column
+Output: the input file, with LICENSE and 506FIELD columns added
+
+## Author
+Rebecca French
